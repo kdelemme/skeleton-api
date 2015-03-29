35ee3b3d82639f1844238cd0c9cb99c3e149d621
Kevin Delemme <kdelemme@gmail.com>
2015-03-29 22:33:04 +0100

    Add index to users.email and tokens.token

--- migrations/index.js
+++ migrations/index.js
@@ -11,7 +11,7 @@ knex.schema.hasTable('users')
     if (!exists) {
       return knex.schema.createTable('users', function(t) {
         t.bigIncrements('id').primary();
-        t.string('email', 100);
+        t.string('email', 100).index('users_email_idx');
         t.string('password', 255);
         t.dateTime('created_at').defaultTo(knex.raw('now()'));
         t.dateTime('updated_at').defaultTo(knex.raw('now()'));
@@ -25,8 +25,8 @@ knex.schema.hasTable('users')
         return knex.schema.createTable('tokens', function (t) {
           t.bigIncrements('id').primary();
           t.biginteger('user_id').references('id').inTable('users');
-          t.string('token', 255);
-          t.timestamp('expiry');
+          t.string('token', 255).index('tokens_token_idx');
+          t.dateTime('expiry');
           t.dateTime('created_at').defaultTo(knex.raw('now()'));
           t.dateTime('updated_at').defaultTo(knex.raw('now()'));
         });
