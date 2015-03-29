27d533970c9f3e76401825c3d02070e4e72650a3
Kevin Delemme <kdelemme@gmail.com>
2015-03-29 15:53:39 +0100

    Rename app

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ var server = app.listen(config.port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Skeleton API listening at http://%s:%s', host, port);
 });
